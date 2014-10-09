5d293838b23906c917cf7d47b78cb24c44a3e5fd
btracey <bdtinternet@gmail.com>
2014-10-08 20:28:56 -0700

    moved files to subfolder

--- nettrainbench/src/github.com/gonum/blas/dbw/impl.go
+++
@@ -1,9 +0,0 @@
-package dbw
-
-import "github.com/gonum/blas"
-
-var impl blas.Float64
-
-func Register(i blas.Float64) {
-	impl = i
-}
--- nettrainbench/src/github.com/gonum/blas/zbw/impl.go
+++
@@ -1,9 +0,0 @@
-package zbw
-
-import "github.com/gonum/blas"
-
-var impl blas.Complex128
-
-func Register(i blas.Complex128) {
-	impl = i
-}
